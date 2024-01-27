5eab35cd4730c54dfe22b0b3085c58c5799c6a21
Harsha S <157780689+harshasnayaka@users.noreply.github.com>
2024-01-27 09:33:46 +0530

    Add files via upload

---
+++ Pro .py
@@ -0,0 +1,12 @@
+print("Welcome to the Google ! : ")
+signin = input("Want to Sign in ? : ")
+if signin == "Yes":
+    print("Signing In it takes a moment ! ")
+else:
+    print("We cant go back ! ")
+username = input("Enter Your Username :")
+Password = int(input("Enter your Password : "))
+if Password == 123456:
+    print("Welcome Harsha S Nayaka ! ")
+else : 
+    print("Invalid Password ")
